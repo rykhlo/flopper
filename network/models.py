1de44027706c7bb136cf6295590f7b3b61ea7681
rykhlo <iurii.rykhlo@utexas.edu>
2021-06-01 15:25:23 -0500

    added models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,45 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ManyToManyField(User, related_name="following")
+    followers = models.ManyToManyField(User, related_name="followers")
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "username" : self.user.username,
+            "following" : [user.username for user in self.following.all()],
+            "followers" : [user.username for user in self.followers.all()],
+        }
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    text = models.CharField(max_length=254)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="liked_posts")
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "author" : self.author.user.username,
+            "text" : self.text,
+            "timestamp" : self.timestamp,
+            "likes" : [user.username for user in self.likes.all()],
+        }
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.CharField(max_length=254)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "author" : self.author.user.username,
+            "post" : self.post,
+            "text" : self.text,
+            "timestamp" : self.timestamp,
+        }
+
+
